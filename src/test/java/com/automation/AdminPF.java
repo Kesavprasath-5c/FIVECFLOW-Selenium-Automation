201b84756a6ddcf674d1091623c6c29a0d44800d
Kesavprasath-5c <kesav@5cnetwork.com>
2025-09-24 17:28:32 +0530

    fixing the project allignment issue

--- src/test/java/com/automation/AdminPF.java
+++ src/test/java/com/automation/AdminPF.java
@@ -2,7 +2,6 @@ package com.automation;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.time.Duration;
 import org.openqa.selenium.By;
